29292493bd347931c2697741a7123f1529e6b708
julianmnst <julians.mg@gmail.com>
2019-06-02 03:09:12 -0300

    Now reveal spreads

--- src/store.js
+++ src/store.js
@@ -71,36 +71,36 @@ function getAdjacentCoordinates({ row, col }) {
 
 	// Check if cell has upper row
 	if (row > 0) {
-		coordinates.push([row - 1, col]);
+		coordinates.push({row: row - 1, col});
 
 		if (col > 0) {
-			coordinates.push([row - 1, col - 1]);
+			coordinates.push({row: row - 1, col: col - 1});
 		}
 
 		if (col < COLS - 1) {
-			coordinates.push([row - 1, col + 1]);
+			coordinates.push({row: row - 1, col: col + 1});
 		}
 	}
 
 	//  Check center row
 	if (col > 0) {
-		coordinates.push([row, col - 1]);
+		coordinates.push({row: row, col: col - 1});
 	}
 
 	if (col < COLS - 1) {
-		coordinates.push([row, col + 1]);
+		coordinates.push({row: row, col: col + 1});
 	}
 
 	// Check lower row
 	if (row < ROWS - 1) {
-		coordinates.push([row + 1, col]);
+		coordinates.push({row: row + 1, col});
 
 		if (col > 0) {
-			coordinates.push([row + 1, col - 1]);
+			coordinates.push({row: row + 1, col: col - 1});
 		}
 
 		if (col < COLS - 1) {
-			coordinates.push([row + 1, col + 1]);
+			coordinates.push({row: row + 1, col: col + 1});
 		}
 	}
 
@@ -110,8 +110,8 @@ function getAdjacentCoordinates({ row, col }) {
 function hasAdjacentMines(cellsToReveal, state) {
 	let hasMines = false
 
-	cellsToReveal.map(coord => {
-		if (state.cells[coord[0]][coord[1]].mined) {
+	cellsToReveal.map(({row, col}) => {
+		if (state.cells[row][col].mined) {
 			hasMines = true;
 		}
 	});
@@ -119,13 +119,27 @@ function hasAdjacentMines(cellsToReveal, state) {
 	return hasMines;
 }
 
-function revealAll(cellsToReveal, state) {
-
-	if (! hasAdjacentMines(cellsToReveal, state)) {
-		cellsToReveal.map(coord => {
-			state.cells[coord[0]][coord[1]].revealed = true
-		})
+function revealAll(state, {row, col}) {
+	let adjacentCells = getAdjacentCoordinates({row, col});
+	if (hasAdjacentMines(adjacentCells, state)) {
+		return false;
 	}
+	else {
+		revealAdjacents(state, {row, col})
+	}
+
+	for (let i = 0; i < adjacentCells.length; i++) {
+	  	if (!hasAdjacentMines(getAdjacentCoordinates(adjacentCells[i]), state)) {
+	  		revealAdjacents(state, adjacentCells[i]);
+	  	}
+	}
+}
+
+function revealAdjacents(state, {row, col}) {
+	let adjacentCells = getAdjacentCoordinates({row, col});
+	adjacentCells.map(cell => {
+		state.cells[cell.row][cell.col].revealed = true;
+	});
 }
 
 /**
@@ -141,8 +155,7 @@ export default new Vuex.Store({
   		state.cells[row][col].revealed = true;
   	},
   	revealAdjacents(state, {row, col}) {
-  		let adjacentCoords = getAdjacentCoordinates({row, col});
-  		revealAll(adjacentCoords, state)
+  		revealAll(state, {row, col})
   	},
   	revealAll(state) {
   		state.cells.map(row => {
